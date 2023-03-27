36f04809aa66c52e7f183cec8ae4fbfe16633946
Denis <den.dvornikov.88@inbox.ru>
2023-03-27 12:06:36 +0600

    Выполнил первое задание

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите числа ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int n =0;
+for (int i = 0; i < size.Length; i++)
+{
+    if(size[i]>0){
+        n++;
+    }
+}
+System.Console.WriteLine($"Положительных чисел введенно: {n}");
